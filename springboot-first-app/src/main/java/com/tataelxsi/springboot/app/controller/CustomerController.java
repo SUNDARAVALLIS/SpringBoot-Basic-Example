b62efe6e178b71a535f18926c4a2ad3bffd0c20c
SUNDARAVALLIS <113921048+SUNDARAVALLIS@users.noreply.github.com>
2024-08-21 08:56:32 +0530

    Add files via upload

---
+++ springboot-first-app/src/main/java/com/tataelxsi/springboot/app/controller/CustomerController.java
@@ -0,0 +1,35 @@
+package com.tataelxsi.springboot.app.controller;
+	import org.springframework.web.bind.annotation.DeleteMapping;
+	import org.springframework.web.bind.annotation.GetMapping;
+	import org.springframework.web.bind.annotation.PostMapping;
+	import org.springframework.web.bind.annotation.PutMapping;
+	import org.springframework.web.bind.annotation.RequestBody;
+	import org.springframework.web.bind.annotation.RequestMapping;
+	import org.springframework.web.bind.annotation.RestController;
+
+import com.tataelxsi.springboot.app.service.CustomerDetails;
+
+	@RestController
+	@RequestMapping("/customerdetails")
+	public class CustomerController {
+		CustomerDetails customer;
+	   @GetMapping("{customerName}")
+	   public CustomerDetails getCustomerDetails(String customerId) {
+		return customer;
+	   }
+	   @PostMapping
+	   public String createCustomerDetails(@RequestBody CustomerDetails customer) {
+		   this.customer = customer;
+		   return "The details were created successfully";
+	   }
+	   @PutMapping
+	   public String updateCustomerDetails(@RequestBody CustomerDetails customer) {
+		   this.customer = customer;
+		   return "The details were updated successfully";
+	   }
+	   @DeleteMapping("{customerName}")
+	   public String deleteCustomerDetails() {
+		   this.customer = null;
+		   return "The details were deleted successfully";
+	   }
+	}
